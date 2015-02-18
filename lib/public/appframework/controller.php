3423a8e7f7e48734ebf8b88c67371d8c7a664820
Bernhard Posselt <dev@bernhard-posselt.com>
2015-02-18 15:31:05 +0100

    remove deprecated controller methods

--- lib/public/appframework/controller.php
+++ lib/public/appframework/controller.php
@@ -50,33 +50,17 @@ abstract class Controller {
 	 */
 	protected $request;
 
-	private $responders;
+	private $responders = [];
 
 	/**
 	 * constructor of the controller
+	 * @deprecated
 	 * @param string $appName the name of the app
 	 * @param IRequest $request an instance of the request
 	 */
-	public function __construct($appName,
-	                            IRequest $request){
+	public function __construct($appName = '', IRequest $request = null){
 		$this->appName = $appName;
 		$this->request = $request;
-
-		// default responders
-		$this->responders = array(
-			'json' => function ($data) {
-				if ($data instanceof DataResponse) {
-					$response = new JSONResponse(
-						$data->getData(),
-						$data->getStatus()
-					);
-					$response->setHeaders(array_merge($data->getHeaders(), $response->getHeaders()));
-					return $response;
-				} else {
-					return new JSONResponse($data);
-				}
-			}
-		);
 	}
 
 
@@ -123,6 +107,24 @@ abstract class Controller {
 	 * @return Response
 	 */
 	public function buildResponse($response, $format='json') {
+		// register default responders
+		if ($format === 'json' && !isset($this->responders['json'])) {
+			$this->responders['json'] = function ($data) {
+				if ($data instanceof DataResponse) {
+					$response = new JSONResponse(
+						$data->getData(),
+						$data->getStatus()
+					);
+					$response->setHeaders(array_merge(
+						$data->getHeaders(), $response->getHeaders()
+					));
+					return $response;
+				} else {
+					return new JSONResponse($data);
+				}
+			};
+		}
+
 		if(array_key_exists($format, $this->responders)) {
 
 			$responder = $this->responders[$format];
@@ -133,101 +135,6 @@ abstract class Controller {
 			throw new \DomainException('No responder registered for format ' .
 				$format . '!');
 		}
-	}
-
-
-	/**
-	 * Lets you access post and get parameters by the index
-	 * @deprecated write your parameters as method arguments instead
-	 * @param string $key the key which you want to access in the URL Parameter
-	 *                     placeholder, $_POST or $_GET array.
-	 *                     The priority how they're returned is the following:
-	 *                     1. URL parameters
-	 *                     2. POST parameters
-	 *                     3. GET parameters
-	 * @param string $default If the key is not found, this value will be returned
-	 * @return mixed the content of the array
-	 */
-	public function params($key, $default=null){
-		return $this->request->getParam($key, $default);
-	}
-
-
-	/**
-	 * Returns all params that were received, be it from the request
-	 * (as GET or POST) or through the URL by the route
-	 * @deprecated use $this->request instead
-	 * @return array the array with all parameters
-	 */
-	public function getParams() {
-		return $this->request->getParams();
-	}
-
-
-	/**
-	 * Returns the method of the request
-	 * @deprecated use $this->request instead
-	 * @return string the method of the request (POST, GET, etc)
-	 */
-	public function method() {
-		return $this->request->getMethod();
-	}
-
-
-	/**
-	 * Shortcut for accessing an uploaded file through the $_FILES array
-	 * @deprecated use $this->request instead
-	 * @param string $key the key that will be taken from the $_FILES array
-	 * @return array the file in the $_FILES element
-	 */
-	public function getUploadedFile($key) {
-		return $this->request->getUploadedFile($key);
-	}
-
-
-	/**
-	 * Shortcut for getting env variables
-	 * @deprecated use $this->request instead
-	 * @param string $key the key that will be taken from the $_ENV array
-	 * @return array the value in the $_ENV element
-	 */
-	public function env($key) {
-		return $this->request->getEnv($key);
-	}
-
-
-	/**
-	 * Shortcut for getting cookie variables
-	 * @deprecated use $this->request instead
-	 * @param string $key the key that will be taken from the $_COOKIE array
-	 * @return array the value in the $_COOKIE element
-	 */
-	public function cookie($key) {
-		return $this->request->getCookie($key);
-	}
-
-
-	/**
-	 * Shortcut for rendering a template
-	 * @deprecated return a template response instead
-	 * @param string $templateName the name of the template
-	 * @param array $params the template parameters in key => value structure
-	 * @param string $renderAs user renders a full page, blank only your template
-	 *                          admin an entry in the admin settings
-	 * @param string[] $headers set additional headers in name/value pairs
-	 * @return \OCP\AppFramework\Http\TemplateResponse containing the page
-	 */
-	public function render($templateName, array $params=array(),
-							$renderAs='user', array $headers=array()){
-		$response = new TemplateResponse($this->appName, $templateName);
-		$response->setParams($params);
-		$response->renderAs($renderAs);
-
-		foreach($headers as $name => $value){
-			$response->addHeader($name, $value);
-		}
-
-		return $response;
 	}
 
 
