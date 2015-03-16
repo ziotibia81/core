23e23a3cc6d02c63e2dc91b00071ded52283e4ef
Felix Böhm <felixboehm@gmx.de>
2015-03-16 14:33:56 +0100

    test lookup for domain request

---
+++ lookup.php
@@ -0,0 +1,27 @@
+<?php
+
+$request = $_POST['request'];
+
+if ($request == "domain") {
+	$username = $_POST['username'];
+
+	$mail = explode("@",$username);
+	if ($mail[1]) {
+		$mail = explode(".", $mail[1]);
+		array_pop($mail);
+		$mail = implode(".", $mail);
+	}
+	$base_domain = "achernar.uberspace.de/oc8ee";
+	$domain = "https://" . $mail . "." . $base_domain;
+	
+	// $domain = "https://owncloud.achernar.uberspace.de/oc8ee";
+
+	echo json_encode(array('domain' => $domain));
+	exit();
+	
+} else {
+	http_response_code(422);
+	echo json_encode(array('error' => array('message' => 'Request not supported.')));
+	exit();
+}
+
