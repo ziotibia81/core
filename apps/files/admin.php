360be27031c4b76ab755a002fee68f445f551697
Joas Schilling <nickvergessen@owncloud.com>
2015-03-17 09:53:30 +0100

    Add a dropdown with the unit of the max upload size

--- apps/files/admin.php
+++ apps/files/admin.php
@@ -22,26 +22,35 @@
 */
 
 OCP\User::checkAdminUser();
+$l = \OC_L10N::get('files');
 
-$htaccessWorking=(getenv('htaccessWorking')=='true');
+$htaccessWorking = getenv('htaccessWorking') == 'true';
+$uploadChangable = $htaccessWorking && is_writable(OC::$SERVERROOT.'/.htaccess');
+$maxFileSizeUnits = [
+	'B' => $l->t('B'),
+	'kB' => $l->t('kB'),
+	'MB' => $l->t('MB'),
+	'GB' => $l->t('GB'),
+	'TB' => $l->t('TB'),
+];
 
-$upload_max_filesize = OCP\Util::computerFileSize(ini_get('upload_max_filesize'));
-$post_max_size = OCP\Util::computerFileSize(ini_get('post_max_size'));
-$maxUploadFilesize = OCP\Util::humanFileSize(min($upload_max_filesize, $post_max_size));
-if($_POST && OC_Util::isCallRegistered()) {
-	if(isset($_POST['maxUploadSize'])) {
-		if(($setMaxSize = OC_Files::setUploadLimit(OCP\Util::computerFileSize($_POST['maxUploadSize']))) !== false) {
-			$maxUploadFilesize = OCP\Util::humanFileSize($setMaxSize);
-		}
+$phpIni = new \bantu\IniGetWrapper\IniGetWrapper();
+$maxUploadFileSize = min($phpIni->getBytes('upload_max_filesize'), $phpIni->getBytes('post_max_size'));
+if ($uploadChangable && isset($_POST['maxUploadSizeValue']) && OC_Util::isCallRegistered()) {
+	$value = ((int) $_POST['maxUploadSizeValue']) . ' ';
+	$value .= (isset($maxFileSizeUnits[(string) $_POST['maxUploadSizeUnit']])) ? $maxFileSizeUnits[(string) $_POST['maxUploadSizeUnit']] : 'B';
+	if (($setMaxSize = OC_Files::setUploadLimit(OCP\Util::computerFileSize($value))) !== false) {
+		$maxUploadFileSize = $setMaxSize;
 	}
 }
-
-$htaccessWritable=is_writable(OC::$SERVERROOT.'/.htaccess');
+list($maxUploadFileSizeValue, $maxUploadFileSizeUnit) = explode(' ', \OCP\Util::humanFileSize($maxUploadFileSize));
 
 $tmpl = new OCP\Template( 'files', 'admin' );
-$tmpl->assign( 'uploadChangable', $htaccessWorking and $htaccessWritable );
-$tmpl->assign( 'uploadMaxFilesize', $maxUploadFilesize);
+$tmpl->assign('uploadChangable', $uploadChangable);
+$tmpl->assign('uploadMaxFileSizeValue', $maxUploadFileSizeValue);
+$tmpl->assign('uploadMaxFileSizeUnit', $maxUploadFileSizeUnit);
+$tmpl->assign('maxFileSizeUnits', $maxFileSizeUnits);
 // max possible makes only sense on a 32 bit system
-$tmpl->assign( 'displayMaxPossibleUploadSize', PHP_INT_SIZE===4);
-$tmpl->assign( 'maxPossibleUploadSize', OCP\Util::humanFileSize(PHP_INT_MAX));
+$tmpl->assign('displayMaxPossibleUploadSize', PHP_INT_SIZE===4);
+$tmpl->assign('maxPossibleUploadSize', OCP\Util::humanFileSize(PHP_INT_MAX));
 return $tmpl->fetchPage();
