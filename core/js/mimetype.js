0bf84e76ab14f6065ffa1beb2324e6133371d026
Roeland Jago Douma <roeland@famdouma.nl>
2015-06-03 16:32:11 +0200

    Less calls to the server in mimetype.js
    Now we just load 1 big list of aliases and the mimetype icons we have.
    This avoids lookups for each mime type. And speeds things up!

--- core/js/mimetype.js
+++ core/js/mimetype.js
@@ -1,85 +1,20 @@
 OC.MimeType = {
 
+	files: [],
+
 	mimeTypeAlias: {},
 
 	mimeTypeIcons: {},
 	
 	init: function() {
-		OC.MimeType.mimeTypeAlias = {
-			"application/octet-stream" : "file", // use file icon as fallback
-
-			"application/illustrator" : "image/vector",
-			"application/postscript" : "image/vector",
-			"image/svg+xml" : "image/vector",
-
-			"application/coreldraw" : "image",
-			"application/x-gimp" : "image",
-			"application/x-photoshop" : "image",
-			"application/x-dcraw" : "image",
-
-			"application/font-sfnt" : "font",
-			"application/x-font" : "font",
-			"application/font-woff" : "font",
-			"application/vnd.ms-fontobject" : "font",
-
-			"application/json" : "text/code",
-			"application/x-perl" : "text/code",
-			"application/x-php" : "text/code",
-			"text/x-shellscript" : "text/code",
-			"application/yaml" : "text/code",
-			"application/xml" : "text/html",
-			"text/css" : "text/code",
-			"application/x-tex" : "text",
-
-			"application/x-compressed" : "package/x-generic",
-			"application/x-7z-compressed" : "package/x-generic",
-			"application/x-deb" : "package/x-generic",
-			"application/x-gzip" : "package/x-generic",
-			"application/x-rar-compressed" : "package/x-generic",
-			"application/x-tar" : "package/x-generic",
-			"application/vnd.android.package-archive" : "package/x-generic",
-			"application/zip" : "package/x-generic",
-
-			"application/msword" : "x-office/document",
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" : "x-office/document",
-			"application/vnd.openxmlformats-officedocument.wordprocessingml.template" : "x-office/document",
-			"application/vnd.ms-word.document.macroEnabled.12" : "x-office/document",
-			"application/vnd.ms-word.template.macroEnabled.12" : "x-office/document",
-			"application/vnd.oasis.opendocument.text" : "x-office/document",
-			"application/vnd.oasis.opendocument.text-template" : "x-office/document",
-			"application/vnd.oasis.opendocument.text-web" : "x-office/document",
-			"application/vnd.oasis.opendocument.text-master" : "x-office/document",
-
-			"application/mspowerpoint" : "x-office/presentation",
-			"application/vnd.ms-powerpoint" : "x-office/presentation",
-			"application/vnd.openxmlformats-officedocument.presentationml.presentation" : "x-office/presentation",
-			"application/vnd.openxmlformats-officedocument.presentationml.template" : "x-office/presentation",
-			"application/vnd.openxmlformats-officedocument.presentationml.slideshow" : "x-office/presentation",
-			"application/vnd.ms-powerpoint.addin.macroEnabled.12" : "x-office/presentation",
-			"application/vnd.ms-powerpoint.presentation.macroEnabled.12" : "x-office/presentation",
-			"application/vnd.ms-powerpoint.template.macroEnabled.12" : "x-office/presentation",
-			"application/vnd.ms-powerpoint.slideshow.macroEnabled.12" : "x-office/presentation",
-			"application/vnd.oasis.opendocument.presentation" : "x-office/presentation",
-			"application/vnd.oasis.opendocument.presentation-template" : "x-office/presentation",
-
-			"application/msexcel" : "x-office/spreadsheet",
-			"application/vnd.ms-excel" : "x-office/spreadsheet",
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" : "x-office/spreadsheet",
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.template" : "x-office/spreadsheet",
-			"application/vnd.ms-excel.sheet.macroEnabled.12" : "x-office/spreadsheet",
-			"application/vnd.ms-excel.template.macroEnabled.12" : "x-office/spreadsheet",
-			"application/vnd.ms-excel.addin.macroEnabled.12" : "x-office/spreadsheet",
-			"application/vnd.ms-excel.sheet.binary.macroEnabled.12" : "x-office/spreadsheet",
-			"application/vnd.oasis.opendocument.spreadsheet" : "x-office/spreadsheet",
-			"application/vnd.oasis.opendocument.spreadsheet-template" : "x-office/spreadsheet",
-			"text/csv" : "x-office/spreadsheet",
-
-			"application/msaccess" : "database"
-		};
+		$.getJSON(OC.webroot + '/core/mimetypes.json', function(data) {
+			OC.MimeType.mimeTypeAlias = data['aliases'];
+			OC.MimeType.files = data['files'];
+		});
 	},
 
 	mimetypeIcon: function(mimeType) {
-		if (mimeType === undefined) {
+		if (_.isUndefined(mimeType)) {
 			return undefined;
 		}
 
@@ -90,49 +25,36 @@ OC.MimeType = {
 			return OC.MimeType.mimeTypeIcons[mimeType];
 		}
 
-		if (mimeType == 'dir') {
-			return OC.webroot + '/core/img/filetypes/folder.png';
-		}
-		if (mimeType == 'dir-shared') {
-			return OC.webroot + '/core/img/filetypes/folder-shared.png';
-		}
-		if (mimeType == 'dir-external') {
-			return OC.webroot + '/core/img/filetypes/folder-external.png';
-		}
-
-		function checkExists(url) {
-			var ok;
-			$.ajax({
-				url:url,
-				async: false,
-				type:'HEAD',
-				error: function() {
-					ok = false;
-				},
-				success: function() {
-					ok = true;
-				}
-			});
-			return ok;
-		}
-
-
 		var icon = mimeType.replace(new RegExp('/', 'g'), '-');
 		//icon = icon.replace(new RegExp('\\', 'g'), '-');
 
-		if (checkExists(OC.webroot + '/core/img/filetypes/' + icon + '.png')) {
-			OC.MimeType.mimeTypeIcons[mimeType] = OC.webroot + '/core/img/filetypes/' + icon + '.png';
-			return OC.MimeType.mimeTypeIcons[mimeType];
+		var path = OC.webroot + '/core/img/filetypes/';
+
+		// Generate path
+		if (mimeType === 'dir') {
+			path += 'folder';
+		} else if (mimeType === 'dir-shared') {
+			path += 'folder-shared';
+		} else if (mimeType === 'dir-external') {
+			path += 'folder-external';
+		} else if ($.inArray(icon, OC.MimeType.files)) {
+			path += icon;
+		} else if ($.inArray(icon.split('-')[0], OC.MimeType.files)) {
+			path += icon.split('-')[0];
+		} else {
+			path += 'file';
 		}
 
-		mimePart = icon.split('-')[0];
-		if (checkExists(OC.webroot + '/core/img/filetypes/' + mimePart + '.png')) {
-			OC.MimeType.mimeTypeIcons[mimeType] = OC.webroot + '/core/img/filetypes/' + mimePart + '.png';
-			return OC.MimeType.mimeTypeIcons[mimeType];
+		// Use svg if we can
+		if(OC.Util.hasSVGSupport()){
+			path += '.svg';
 		} else {
-			OC.MimeType.mimeTypeIcons[mimeType] = OC.webroot + '/core/img/filetypes/file.png';
-			return OC.MimeType.mimeTypeIcons[mimeType];
+			path += '.png';
 		}
+
+		// Cache the result
+		OC.MimeType.mimeTypeIcons[mimeType] = path;
+		return path;
 	}
 
 };
